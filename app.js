cfad7cf78a2e2e002a92e80d9b0df210004f6ed5
phrubel <phrubel42@gmail.com>
2021-02-07 04:47:32 +0600

    Quarter Done

--- app.js
+++ app.js
@@ -1,33 +1,53 @@
-// Search Button Event Handler
+// Search Button Handler
 document.getElementById("search-button").addEventListener("click", () => {
-  document.getElementById("food-input").value;
+  const foodInput = document.getElementById("food-input").value;
+
+  // Api Call
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${foodInput}`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("foods").innerHTML = "";
+      document.getElementById("foods-details").innerHTML = "";
+      const foods = document.getElementById("foods");
+      data.meals.forEach((foodElements) => {
+        const food = document.createElement("div");
+        food.innerHTML = `<img src="${foodElements.strMealThumb}" onclick="getFoodsDetails(${foodElements.idmeal})">
+        <h2onclick="getFoodsDetails(${foodElements.idmeal})">${foodElements.strMeal}</h2>`;
+        foods.appendChild(food);
+      });
+    })
+
+    // Error handling
+    .catch((error) => {
+      console.log(error);
+      document.getElementById("foods").innerHTML = "";
+      document.getElementById("foods-details").innerHTML = "";
+      const foods = document.getElementById("foods");
+      const wrong = document.createElement("h3");
+      wrong.innerHTML = `Something Wrong....please try again!!!!`;
+      foods.appendChild(wrong);
+    });
 });
 
-// call api
-fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
- .then(response=>response.json())
-  .then(data => displayFood(data));
-
-// Display Food
-const displayFood = (foods) => {
-  const foodsDiv = document.getElementById("foods");
-  foods.forEach(food => {
-    const foodDiv = document.createElement("div");
-    foodDiv.className = "food";
-    const foodsInfo = `<h4>${strMeal.name} </h4>`;
-    foodDiv.innerHtml = foodsInfo;
-    foodsDiv.appendChild(foodDiv);
-  });
+// food details Click handler
+const getFoodsDetails = (lookupFoodId) => {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${lookupFoodId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const foodsDetails = document.getElementById("foods-details");
+      document.getElementById("foods-details").innerHTML = "";
+      //   document.getElementById("foods-details").style.display = "block";
+      const foodDetail = documentt.createElement("div");
+      foodDetail.innerHTML = `<img src="${data.meals[0].strMealThumb}">
+        <h2>${data.meals[0].strMeal} </h2>
+        <h2>Catagories:${data.meals[0].strCategory} </h2>
+        
+        <h4>${data.meals[0].strMeasure1}</h4>
+        <h4>${data.meals[0].strMeasure2}</h4>
+        <h4>${data.meals[0].strMeasure3}</h4>
+        <h4>${data.meals[0].strMeasure4}</h4>
+        <h4>${data.meals[0].strMeasure5}</h4>
+        <h4>${data.meals[0].strMeasure6}</h4>`;
+      foodsDetails.appendChild(foodDetail);
+    });
 };
-
-// fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
-//   .then((response) => response.json())
-//   .then(data=>{
-//       console.log(data);
-//   })
-
-// fetch('https://restcountries.eu/rest/v2/all')
-// .then(res => res.json())
-// .then(data => {
-//     console.log(data);
-// });
